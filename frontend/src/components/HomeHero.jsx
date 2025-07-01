7672d616c9924a0178e9c3f92c206db859ca5c8b
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2025-07-01 23:40:02 +0530

    test vite and tailwinds

--- frontend/src/components/HomeHero.jsx
+++ frontend/src/components/HomeHero.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 
 function HomeHero() {
   return (
-    <div
-    className="bg-cover bg-center lg:h-[800px] flex items-center justify-center "
-    style={{
-      backgroundImage: "url('homee.png')", 
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}>
-    
-   </div>
+  <div className="bg-gradient-to-r from-amber-600 to-purple-500 text-white p-8 rounded-lg shadow-lg flex flex-col items-center justify-center text-center">
+    {/* Add your hero content here */}
+  </div>
   
   )
 }
